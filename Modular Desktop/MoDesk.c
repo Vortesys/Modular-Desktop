5924cf040da57bf7286627258b0c72959b3c34fa
freedom <freedom7341@hotmail.com>
2023-12-22 16:55:04 -0700

    initial v2

---
+++ Modular Desktop/MoDesk.c
@@ -0,0 +1,8 @@
+/* * * * * * * *\
+	MoDesk.C -
+		Copyright © 2023 Brady McDermott, Freedom Desktop
+	DESCRIPTION -
+		Modular Desktop's main file, including WinMain!
+	LICENSE INFORMATION - 
+		MIT License, see LICENSE.txt in the root folder
+ \* * * * * * * */
