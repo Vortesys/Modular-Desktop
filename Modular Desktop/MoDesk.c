bd16e125d4994a7e55ec85a86826f02892dedf7b
Brady McDermott <freedom7341@hotmail.com>
2023-12-27 02:55:01 -0700

    because you, you got what i need...

--- Modular Desktop/MoDesk.c
+++
@@ -1,8 +0,0 @@
-/* * * * * * * *\
-	MoDesk.C -
-		Copyright © 2023 Brady McDermott, Freedom Desktop
-	DESCRIPTION -
-		Modular Desktop's main file, including WinMain!
-	LICENSE INFORMATION - 
-		MIT License, see LICENSE.txt in the root folder
- \* * * * * * * */
